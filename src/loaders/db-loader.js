0e6ff9d826cd260e2768628141b04ae007c9b4dd
lbertoncello <lucas_b_andrade@hotmail.com>
2023-05-05 19:58:56 -0300

    feat: add new options to mongodb connection string

--- src/loaders/db-loader.js
+++ src/loaders/db-loader.js
@@ -4,9 +4,7 @@ import options from '../config/config.js';
 
 export default async (url = options.dbUrl, opts = {}) => {
   try {
-    const dbOptions = { ...opts };
-    console.log('url: ', url);
-    console.log('dbOptions: ', dbOptions);
+    const dbOptions = { ...opts, useNewUrlParser: true, useUnifiedTopology: true };
     await mongoose.connect(url, dbOptions);
     const connection = mongoose.connection;
   
