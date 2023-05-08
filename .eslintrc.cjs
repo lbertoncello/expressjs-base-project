e245f79c77084557a76bd2e511bf557f272cdb3a
lbertoncello <lucas_b_andrade@hotmail.com>
2023-05-08 19:21:45 -0300

    chore: update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
   extends: 'standard',
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
+    'comma-dangle': [ 'error', 'only-multiline' ],
+    semi: [ 'error', 'always', { omitLastInOneLineBlock: true } ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'array-bracket-spacing': [ 'error', 'always' ]
   }
-}
+};
