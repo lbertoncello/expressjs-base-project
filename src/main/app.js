d0ada97ea028ba20e25261af33b11917bca9398b
lbertoncello <lucas_b_andrade@hotmail.com>
2023-05-08 19:42:50 -0300

    refactor: remove comments

--- src/main/app.js
+++ src/main/app.js
@@ -1,17 +1,3 @@
-// import express from 'express';
-// import cookieParser from 'cookie-parser';
-// import routes from '../routes/routes.js';
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-// app.use('/', routes);
-
-// export default app;
-
 import { start } from './server.js';
 
 start();
