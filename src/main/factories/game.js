8d083903099bab00c10acca4a6e68abe90c22fde
lbertoncello <lucas_b_andrade@hotmail.com>
2023-05-11 15:36:46 -0300

    refactor: move game controller instance to a factory

---
+++ src/main/factories/game.js
@@ -0,0 +1,11 @@
+import GameController from '../../presentation/controllers/game-controller.js';
+import GameDatabase from '../../data-access/database/game-database.js';
+import GameRepository from '../../entities/repositories/game-repository.js';
+
+export const makeGameController = () => {
+  const database = new GameDatabase();
+  const repository = new GameRepository(database);
+  const controller = new GameController(repository);
+
+  return controller;
+};
--- src/main/routes/game-routes.js
+++ src/main/routes/game-routes.js
@@ -1,13 +1,9 @@
 import express from 'express';
 import { adaptRoute } from '../adapters/express-router-adapter.js';
-import GameController from '../../presentation/controllers/game-controller.js';
-import GameDatabase from '../../data-access/database/game-database.js';
-import GameRepository from '../../entities/repositories/game-repository.js';
+import { makeGameController } from '../factories/game.js';
 
 const router = express.Router();
-const database = new GameDatabase();
-const repository = new GameRepository(database);
-const controller = new GameController(repository);
+const controller = makeGameController();
 
 router.route('/game').post(adaptRoute(controller.addGame.bind(controller)));
 
