f68d10432d8a2745caef8ddb8e67f1f9bed7a23f
lbertoncello <lucas_b_andrade@hotmail.com>
2023-05-16 17:01:21 -0300

    refactor: change UpdateGame use case to filter allowed fields

--- src/use-cases/game/update-game.js
+++ src/use-cases/game/update-game.js
@@ -1,10 +1,14 @@
+import fieldsFilter from '../../utils/fields-filter.js';
+
 export default class UpdateGame {
   constructor(repository) {
     this.repository = repository;
   }
 
   async execute(id, fields) {
-    const result = await this.repository.updateById(id, fields);
+    const allowedFieldNames = ['title', 'rating', 'summary'];
+    const filteredFields = fieldsFilter(allowedFieldNames, fields);
+    const result = await this.repository.updateById(id, filteredFields);
 
     return result;
   }
