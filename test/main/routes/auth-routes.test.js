eabb09b1c41de3ade324fa1b69ccb9cd02f33694
lbertoncello <lucas_b_andrade@hotmail.com>
2023-05-19 17:12:05 -0300

    test: ensure change password route successfully change user's password if valid data is provided

--- test/main/routes/auth-routes.test.js
+++ test/main/routes/auth-routes.test.js
@@ -3,6 +3,7 @@ import app from '../../../src/main/app.js';
 import dbLoader from '../../../src/main/loaders/db-loader.js';
 import UserDatabase from '../../../src/data-access/database/user-database.js';
 import JwtAdapter from '../../../src/data-access/auth/jwt-adapter.js';
+import BcrypterAdapter from '../../../src/data-access/auth/bcrypt-adapter.js';
 import envConfig from '../../../src/main/config/env/env.js';
 
 describe('Auth Routes', () => {
@@ -62,5 +63,41 @@ describe('Auth Routes', () => {
     expect(decoded).toBeTruthy();
     expect(decoded.name).toBe('Lucas');
     expect(decoded.email).toBe('lucas@mail.com');
+  });
+
+  test("Should successfully change the user's password if valid data is provided", async () => {
+    await request(app).post('/api/v1/auth/signup').send({
+      name: 'Lucas',
+      email: 'lucas@mail.com',
+      password: '123',
+      passwordConfirmation: '123',
+    });
+    const signInRes = await request(app)
+      .post('/api/v1/auth/signin')
+      .send({
+        email: 'lucas@mail.com',
+        password: '123',
+      })
+      .expect(200);
+    const token = signInRes.body.data.token;
+
+    const changePasswordRes = await request(app)
+      .post('/api/v1/auth/password')
+      .send({
+        password: 'abcd',
+        passwordConfirmation: 'abcd',
+        oldPassword: '123',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+
+    const feedback = changePasswordRes.body.data;
+    const bcrypterAdapter = new BcrypterAdapter();
+    const dbUser = await database.findOne({ email: 'lucas@mail.com' }).select('+password').lean();
+    const passwordMatch = await bcrypterAdapter.compare('abcd', dbUser.password);
+
+    expect(feedback.previousPasswordMatch).toBeTruthy();
+    expect(feedback.updated).toBeTruthy();
+    expect(passwordMatch).toBeTruthy();
   });
 });
