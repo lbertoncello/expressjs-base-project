f96122fcc44b0aa3dd0b0ad7b22a59f9e0193beb
lbertoncello <lucas_b_andrade@hotmail.com>
2023-05-24 12:28:05 -0300

    refactor: change the type of error returned in ChangePasswordController when password does not match

--- src/presentation/controllers/auth/change-password.js
+++ src/presentation/controllers/auth/change-password.js
@@ -2,6 +2,7 @@ import SuccessResponse from '../../responses/success-response.js';
 import InvalidParamError from '../../errors/invalid-param-error.js';
 import MissingParamError from '../../errors/missing-param-error.js';
 import ApplicationError from '../../errors/application-error.js';
+import ClientError from '../../errors/client-error.js';
 
 export default class ChangePasswordController {
   constructor(changePassword) {
@@ -24,7 +25,7 @@ export default class ChangePasswordController {
     const result = await this.changePassword.execute(signedUser, password, oldPassword);
 
     if (!result.previousPasswordMatch)
-      throw new InvalidParamError('The previous password does not match the password provided');
+      throw new ClientError('The previous password does not match the password provided', 401);
 
     if (!result.updated) throw new ApplicationError('It was not possible to change your password');
 
