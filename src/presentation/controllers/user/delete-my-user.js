e51e1a11367020af96547236f6046866b5b2dfc0
lbertoncello <lucas_b_andrade@hotmail.com>
2023-05-24 16:15:45 -0300

    refactor: change type of error thrown by DeleteMyUserController

--- src/presentation/controllers/user/delete-my-user.js
+++ src/presentation/controllers/user/delete-my-user.js
@@ -1,5 +1,5 @@
 import SuccessResponse from '../../responses/success-response.js';
-import ClientError from '../../errors/client-error.js';
+import ApplicationError from '../../errors/application-error.js';
 
 export default class DeleteMyUserController {
   constructor(deleteUser) {
@@ -10,7 +10,7 @@ export default class DeleteMyUserController {
     const signedUser = req.authUser;
     const result = await this.deleteUser.execute(signedUser);
 
-    if (!result.deleted) throw new ClientError('It was not possible to delete the specified record', 400);
+    if (!result.deleted) throw new ApplicationError('It was not possible to delete the specified record', 400);
     return new SuccessResponse(result);
   }
 }
